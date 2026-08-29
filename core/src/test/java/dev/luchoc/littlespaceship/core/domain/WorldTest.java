43995338f9e795811632820bfb54390d9a5b481f
Luciano <154899000+LuchoC-Dev@users.noreply.github.com>
2026-08-29 18:48:32 -0300

    test(core): cover the arc shape and the two-shapes-one-archetype case

--- core/src/test/java/dev/luchoc/littlespaceship/core/domain/WorldTest.java
+++ core/src/test/java/dev/luchoc/littlespaceship/core/domain/WorldTest.java
@@ -122,7 +122,7 @@ class WorldTest {
         world.enemyWeapons().set(entity, new EnemyWeapon("straight-single", 1f, 90f));
         world.lifetimes().set(entity, new Lifetime(1f));
         world.waveOrigins().set(entity, new WaveOrigin("wave-1"));
-        world.trajectories().set(entity, new Trajectory());
+        world.trajectories().set(entity, new Trajectory("slow-descent"));
     }
 
     /**
